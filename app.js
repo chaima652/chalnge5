851f9c3fcd8d432747379c28070ff51edaf442b5
chaima652 <127987214+chaima652@users.noreply.github.com>
2023-06-01 00:13:31 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
+
+      const dismissButton = document.getElementById("dismiss-button");
+      dismissButton.addEventListener("click", function () {
+      window.location.href = document.referrer;
+      });
